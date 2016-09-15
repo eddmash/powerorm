71c6ca76278ec1b15dbc272a9cc00d21223fe223
eddmash <edd.cowan@gmail.com>
2016-09-16 01:55:42 +0300

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,7 +123,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'eddmash_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -132,8 +132,7 @@ html_theme = 'eddmash_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
-html_theme_path = ["./_themes"]
+# html_theme_path = [] 
 
 # Set up PHP syntax highlights
 from sphinx.highlighting import lexers
