f3a17d5deffabe77681120561f3f847184dcd04a
eddmash <edd.cowan@gmail.com>
2016-10-23 16:36:12 +0300

    deleted all cached

--- pmanager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (strtolower(basename(__DIR__)) === 'powerorm'):
-    require 'Application.php';
-
-else:
-    require __DIR__.'/application/libraries/powerorm/Application.php';
-endif;
-Application::run(['baseDir' => __DIR__]);
--- src/Validator/ValidatorBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Created by eddmash <http://eddmash.com>
- * Date: 9/14/16.
- */
-class ValidatorBase
-{
-}
