6132c77a66c1835a53ca32721876b9b658c43c29
eddmash <edd.cowan@gmail.com>
2017-02-16 19:19:37 +0300

    cs format

--- src/Model/Lookup/Related/RelatedGreaterThan.php
+++ src/Model/Lookup/Related/RelatedGreaterThan.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Eddmash\PowerOrm\Model\Lookup\Related;
 
 use Eddmash\PowerOrm\Model\Lookup\GreaterThan;
