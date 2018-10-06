58ea68c8b01f372e04b51cce83306213f0ff338d
eddmash <edd.cowan@gmail.com>
2018-10-06 08:37:04 +0300

    travis mysql fix

--- tests/dbconfigs/mysql-setup.sh
+++ tests/dbconfigs/mysql-setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('root') where user='root'"
-sudo mysql -e "use mysql;update user set plugin='mysql_native_password'"
-sudo mysql -e "use mysql;FLUSH PRIVILEGES;"
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('root') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
 sudo mysql_upgrade -u root -proot
 sudo mysql -u root -proot -e "create database powerormtest;"
 sudo service mysql restart
