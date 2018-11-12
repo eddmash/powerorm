3eff718ce048e05f028249e34c1d49ffdec16903
edd.cowan@gmail.com <edd.cowan@gmail.com>
2018-11-12 11:38:18 +0300

    update makemodel to use app

--- src/Console/Command/MakeModel.php
+++ src/Console/Command/MakeModel.php
@@ -11,10 +11,14 @@
 
 namespace Eddmash\PowerOrm\Console\Command;
 
+use Eddmash\PowerOrm\BaseOrm;
+use Eddmash\PowerOrm\Components\AppInterface;
 use Eddmash\PowerOrm\Exception\CommandError;
+use Eddmash\PowerOrm\Exception\ComponentException;
 use Eddmash\PowerOrm\Helpers\ClassHelper;
 use Eddmash\PowerOrm\Helpers\FileHandler;
 use Eddmash\PowerOrm\Migration\FormatFileContent;
+use Eddmash\PowerOrm\Model\Model;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -30,18 +34,16 @@ class MakeModel extends BaseCommand
             ->setDescription($this->help)
             ->setHelp($this->help)
             ->addArgument(
+                'app_name',
+                InputArgument::REQUIRED,
+                'The app to which this model will belong.',
+                null
+            )
+            ->addArgument(
                 'model_name',
                 InputArgument::REQUIRED,
-                'The name of the model to generate. '.
+                'The name of the model to generate. ' .
                 'Use the name "zero" to unapply all migrations.'
-            )
-            ->addOption(
-                'path',
-                '-p',
-                InputOption::VALUE_OPTIONAL,
-                'The location the generated model will be place relative to vendor folder,
-                 defaults to same level as vendor folder.',
-                null
             )
             ->addOption(
                 'force',
@@ -51,28 +53,42 @@ class MakeModel extends BaseCommand
             );
     }
 
+    /**@inheritdoc */
     public function handle(InputInterface $input, OutputInterface $output)
     {
-        $vendorHome = dirname(dirname(dirname(POWERORM_HOME)));
+//        $vendorHome = dirname(dirname(dirname(POWERORM_HOME)));
 
         $orginalModelName = $modelName = $input->getArgument('model_name');
-        list($namespace, $modelName) = ClassHelper::getNamespaceNamePair($modelName);
-        $modelName = ucfirst($modelName);
-        $modelPath = $input->getOption('path');
 
+        $modelName = ucfirst($modelName);
+        $app_name = $input->getArgument('app_name');
         $force = $input->getOption('force');
 
-        $path = rtrim($vendorHome, '\\').DIRECTORY_SEPARATOR.ltrim($modelPath, '\\');
-        $path = realpath($path);
+        try {
+            $component = BaseOrm::getInstance()->getComponent($app_name);
+        } catch (ComponentException $e) {
+            throw new CommandError(sprintf("Could not find an application with the name %s", $app_name));
+        }
+        if (!$component instanceof AppInterface) {
+            throw new CommandError(sprintf("is not an application %s, it does not implement %s", $app_name,
+                AppInterface::class));
+        }
+
+        $path = $component->getModelsPath();
 
         if (!file_exists($path)) {
             throw new CommandError(sprintf("directory '%s' does not exist", $path));
         }
 
+        // assumes the app is using composer to autload using psr-4
+        $namespace = sprintf("%s\%s", $component->getNamespace(), substr($path,
+            strrpos($path, DIRECTORY_SEPARATOR) + 1));
+        $namespace = ltrim($namespace, "\\");
+
         $modelFile = $this->getModelFile($namespace, $modelName);
         $fileName = rtrim(sprintf('%s.php', $modelName), '\\');
 
-        $filePath = realpath($path.DIRECTORY_SEPARATOR.$fileName);
+        $filePath = realpath($path . DIRECTORY_SEPARATOR . $fileName);
 
         if (file_exists($filePath) && !$force) {
             throw new CommandError(sprintf("Models '%s' already exists, use -f option to overwrite", $filePath));
@@ -81,7 +97,7 @@ class MakeModel extends BaseCommand
         $handler = new FileHandler($path, $fileName);
 
         if ($handler->write($modelFile)) {
-            $output->write(sprintf("Models '%s' created at '%s' ".PHP_EOL, $orginalModelName, $path));
+            $output->write(sprintf("Models '%s' created at '%s' " . PHP_EOL, $orginalModelName, $path));
         }
     }
 
@@ -89,29 +105,28 @@ class MakeModel extends BaseCommand
     {
         $content = FormatFileContent::createObject();
 
-        $content->addItem('<?php'.PHP_EOL);
+        $content->addItem('<?php' . PHP_EOL);
 
         if ($namespace) {
-            $content->addItem(sprintf('namespace %s;', $namespace).PHP_EOL);
+            $content->addItem(sprintf('namespace %s;', $namespace) . PHP_EOL);
         }
 
-        $content->addItem('use Eddmash\\PowerOrm\\Models\\Models;'.PHP_EOL);
+//        $content->addItem(sprintf('use %s;', Model::class) . PHP_EOL);
 
         $content->addItem('/**');
         $content->addItem(sprintf('* Class %s', $modelName));
         $content->addItem('*/');
-        $content->addItem(sprintf('class %s extends Models', $modelName));
+        $content->addItem(sprintf('class %s extends \%s', $modelName, ltrim(Model::class, "\\")));
         $content->addItem('{');
-
         $content->addIndent();
-        $content->addItem('private function unboundFields()');
+        $content->addItem('public function unboundFields()');
         $content->addItem('{');
 
         $content->addIndent();
         $content->addItem('return [];');
         $content->reduceIndent();
 
-        $content->addItem('}'.PHP_EOL);
+        $content->addItem('}' . PHP_EOL);
         $content->reduceIndent();
         $content->addItem('}');
 
